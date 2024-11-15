861d8380d0ab95d37d53608f7b7dcb8ba6585ccc
JinseongMOK <mjs4027@naver.com>
2024-11-15 21:37:05 +0900

    station_list_page ui 완성

--- lib/screen/station_list_page.dart
+++ lib/screen/station_list_page.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget {
-  const StationListPage({super.key});
+  final List<String> stations = [
+    "수서",
+    "동탄",
+    "평택지제",
+    "천안아산",
+    "오송",
+    "대전",
+    "김천구미",
+    "동대구",
+    "경주",
+    "울산",
+    "부산"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +21,30 @@ class StationListPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('출발역'),
       ),
-      body: Text('StationListPage'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView.builder(
+          itemCount: stations.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 50,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    stations[index],
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Divider(color: Colors.grey[300]),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
